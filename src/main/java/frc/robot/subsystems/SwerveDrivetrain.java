0d70e73b745774f825a17a9a3fee33fdca443d9c
FatCactis <crawlingbeetles64@gmail.com>
2024-05-02 16:37:41 -0700

    dependencies install

---
+++ src/main/java/frc/robot/subsystems/SwerveDrivetrain.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.mechanisms.swerve.SimSwerveDrivetrain;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.WheelPositions;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class SwerveDrivetrain extends SubsystemBase {
+   
+}
